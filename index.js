dde603b8ab9a935f34dbd6c248b65733db79f155
dkzhen <dani314123@gmail.com>
2023-08-05 11:37:01 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Load the data from data.json
+const data = require("./data.json");
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Parse JSON data in the request body
+app.use(bodyParser.json());
+
+// Route to get the data in JSON format
+app.get("/api", (req, res) => {
+  res.json(data);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
