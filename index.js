40acdde0cc1560f131d79a0ddbbd88dad3018254
dkzhen <dani314123@gmail.com>
2023-08-09 15:29:08 +0700

    add post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const fs = require("fs"); // Import the fs module
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -14,13 +15,20 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Route to get the data in JSON format
-app.get("/", (req, res) => {
+app.get("/info", (req, res) => {
   res.send("<h3>Welcome to my api [!!!] Api running on port " + PORT + "</h3>");
 });
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.json(data);
 });
-
+// POST endpoint
+// POST endpoint to add data
+app.post("/add", (req, res) => {
+  const newData = req.body;
+  data.push(newData); // Append new data to the existing data array
+  fs.writeFileSync("./data.json", JSON.stringify(data, null, 2));
+  res.json({ message: "Data added successfully!" });
+});
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
